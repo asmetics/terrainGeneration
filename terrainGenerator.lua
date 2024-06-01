621b9947e58e88c6425bdaa1229156d3cc39f626
nuqko <116861388+nuqko@users.noreply.github.com>
2024-06-01 16:48:06 -0500

    Update terrainGenerator.lua

--- terrainGenerator.lua
+++ terrainGenerator.lua
@@ -1,48 +1,80 @@
 local totalNodes = 10000
-local currentNodes = 0
+local startPosition = Vector3.new(0, 0, 0)
+local nodeDistance = 10
+local positions
+RenderSize = 100
+Resolution = 100
+local Frequency = nil
+local Amplitude = nil
+local Resoloution = nil
 
-local startPosition = Vector3.new(0,0,0)
-local nodeDistance = 20
+function newValues()
+	Frequency = math.random(5, 10)
+	Amplitude = math.random(3, 6)
+	Resoloution = math.random(60, 120)
+end
+
+newValues()
 
 local folder = Instance.new("Folder")
 folder.Parent = game.Workspace
 folder.Name = "Nodes"
 
-local raycastParams = RaycastParams.new()
-raycastParams.RespectCanCollide = true
+function spiral(X, Y)
+	local x, y = 0, 0
+	local dx, dy = 0, -1
+	local positions = {}
 
-local function polarToCartesian(radius, angle)
-	return Vector3.new(radius * math.cos(angle), 0, radius * math.sin(angle))
+	for i = 1, (math.max(X, Y))^2 do
+		if (-X/2 < x and x <= X/2) and (-Y/2 < y and y <= Y/2) then
+			table.insert(positions, Vector3.new(x * nodeDistance, startPosition.Y + math.random(-2, 2), y * nodeDistance))
+		end
+		if x == y or (x < 0 and x == -y) or (x > 0 and x == 1 - y) then
+			dx, dy = -dy, dx
+		end
+		x, y = x + dx, y + dy
+	end
+
+	return positions
 end
 
+local function GetHeight(x :number, z :number): number -- Creates Our Function
+	local noiseHeight = math.noise( -- Math.Noise
+		x / Resoloution * Frequency, -- The First Value Is X Divided And Multiplied
+		z / Resoloution * Frequency -- The Second is Z but the same as X
+	)
+	noiseHeight = math.clamp(noiseHeight, -.5, .5) + .5
+	return noiseHeight -- Returns The 'Noise' Height / Value
+end
+
+
 function new()
-	local lastNodePosition = startPosition
-	local angleIncrement = 2 * math.pi / 50 
-	local angle = 10
-	local radius = nodeDistance
+	local currentNodes = 0
+	local _max = 100
+	local max = _max
+	for x = 0, RenderSize do
+		for z = 0, RenderSize do
+			local cell = Instance.new("Part")
+			
+			if max - 1 == 0 then
+				game["Run Service"].Heartbeat:Wait()
+				max = max - 1
+			else
+				max = _max
+			end
+			
+			cell.Anchored = true
+			cell.Parent = folder
+			cell.Size = Vector3.new(10, 50, 10)
+			cell.Color = Color3.fromRGB(62, 106, 35)
+			cell.Material = Enum.Material.Grass
 
-	for currentNodes = 0, totalNodes, 1 do
-		currentNodes = currentNodes + 1
-		radius = totalNodes - currentNodes 
-		local newpart = Instance.new("Part")
-		newpart.Parent = folder
-		newpart.Size = Vector3.new(0.5, 0.5, 0.5)
-		newpart.Material = Enum.Material.Neon
-		newpart.Anchored = true
-
-		local offset = polarToCartesian(radius, angle)
-		newpart.Position = startPosition + offset + Vector3.new(0 + math.random(-radius / 3, radius / 3), lastNodePosition.Y + math.random(-2, 2), 0 + math.random(-radius / 3, radius / 3))
-
-		if game.Workspace:Raycast(newpart.Position, Vector3.new(0, 1000, 0), raycastParams) or game.Workspace:Raycast(newpart.Position, Vector3.new(0, -1000, 0), raycastParams) then
-			newpart:Destroy()
-			currentNodes = currentNodes - 1
-		else
-			lastNodePosition = newpart.Position
-			angle = angle + angleIncrement
-			radius = radius + nodeDistance / (2 * math.pi)
+			local newPosition = Vector3.new(startPosition.X + math.floor(x * cell.Size.X), startPosition.Y + math.floor(GetHeight(x, z) * Amplitude) * Amplitude, startPosition.Z + math.floor(z * cell.Size.X))
+			cell.Position = newPosition
 		end
 	end
 end
+
 
 game.ReplicatedStorage:WaitForChild("new").OnServerEvent:Connect(function()
 	if game.Workspace:FindFirstChild("Nodes") then
@@ -51,4 +83,5 @@ game.ReplicatedStorage:WaitForChild("new").OnServerEvent:Connect(function()
 		end
 	end
 	new()
+	newValues()
 end)
